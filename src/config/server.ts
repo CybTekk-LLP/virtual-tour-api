c375ca7dfb3f5b1ae2f3ce2f3cf3ee0728df274c
Shubhcyb2611 <s.gupta@cybtekk.com>
2024-07-08 13:03:59 +0530

    feat: scripts added

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,59 +1,57 @@
-import { AppDataSource } from '@/infrastructure';
-import express from 'express';
-import { Logger } from './logger';
-import { appRouter } from '@/interfaces/routers';
+import { AppDataSource } from "@/infrastructure";
+import express from "express";
+import { Logger } from "./logger";
+// import { appRouter } from '@/interfaces/routers';
 import {
-    ExpressErrorHandler,
-    corsConfig,
-    createAdminUser,
-    userDeserializer,
-} from '@/interfaces/middleware';
-import cors from 'cors';
-import { UPLOADS_PATH } from './env.config';
+  ExpressErrorHandler,
+  corsConfig,
+  createAdminUser,
+  userDeserializer,
+} from "@/interfaces/middleware";
+import cors from "cors";
+import { UPLOADS_PATH } from "./env.config";
 
 export type AppConfig = {
-    port?: number | string;
+  port?: number | string;
 };
 
 export class Server {
-    private app;
-    private config: AppConfig;
+  private app;
+  private config: AppConfig;
 
-    constructor(config: AppConfig) {
-        this.config = config;
+  constructor(config: AppConfig) {
+    this.config = config;
 
-        this.app = express();
-        this.app.use(express.json());
+    this.app = express();
+    this.app.use(express.json());
 
-        this.app.use(userDeserializer);
-        this.app.use(cors(corsConfig));
-        
-        this.app.use('/api/uploads', express.static(UPLOADS_PATH));
-        this.app.use('/api', appRouter);
+    this.app.use(userDeserializer);
+    this.app.use(cors(corsConfig));
 
-        this.app.use(ExpressErrorHandler);
-    }
-    private connectDatabase() {
-        AppDataSource.initialize()
-            .then(() => {
-                Logger.info('🤠: Database connection instantiated');
-                createAdminUser();
-            })
-            .catch((e) => {
-                Logger.error(e);
-                throw new Error('500::Failed to connect to database');
-            });
-    }
+    this.app.use("/api/uploads", express.static(UPLOADS_PATH));
+    // this.app.use('/api', appRouter);
 
-    start() {
-        const port = this.config.port ?? 1209;
-        this.connectDatabase();
+    this.app.use(ExpressErrorHandler);
+  }
+  private connectDatabase() {
+    AppDataSource.initialize()
+      .then(() => {
+        Logger.info("🤠: Database connection instantiated");
+        createAdminUser();
+      })
+      .catch((e) => {
+        Logger.error(e);
+        throw new Error("500::Failed to connect to database");
+      });
+  }
 
-        this.app.listen(port, () => {
-            Logger.info(`🚀: Server started on http://localhost:` + port);
-            Logger.info(
-                `📝: Serving docs on http://localhost:${port}/api/docs`
-            );
-        });
-    }
+  start() {
+    const port = this.config.port ?? 1209;
+    this.connectDatabase();
+
+    this.app.listen(port, () => {
+      Logger.info(`🚀: Server started on http://localhost:` + port);
+      Logger.info(`📝: Serving docs on http://localhost:${port}/api/docs`);
+    });
+  }
 }
